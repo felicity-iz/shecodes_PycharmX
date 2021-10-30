2cb770e18be4a60fbf39ea82639ca9567d62d339
Omer Izhari <omer@MENTFIELD.CO.IL>
2021-10-30 16:59:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+versions = [1]
+
+
+def print_version():
+    # Use a breakpoint in the code line below to debug your script.
+    latest_version = versions[-1]
+    print(f'this is version {latest_version}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_version()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
